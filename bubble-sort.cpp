20238294a0277c9fa8a596fa360d3818f81ecd35
niciki <71276784+niciki@users.noreply.github.com>
2021-05-30 01:43:40 +0300

    Create bubble-sort.cpp

---
+++ bubble-sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int s, k, p = 0, tmp;
+    for(int i = 1; i < n; i++){
+        for(int j = n - 1; j > p; j--){
+            if(a[j] < a[j - 1]){
+                tmp = a[j - 1];
+                a[j - 1] = a[j];
+                a[j] = tmp;
+            }
+        }
+        p++;
+    }
+    for(int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    return 0;
+}
